66c01d8b93767fbf2f8d315c84dd65484fa5c4d4
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-02 12:00:14 +0900

    settings division(dev/production/base), STATIC_ROOT added

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.admin import site
+
+from .models import User
+
+admin.site.register(User)
--- app/members/models.py
+++ app/members/models.py
@@ -4,4 +4,9 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    # admin에서 image수정할 수 있도록 설정 -> members/admin.py
+
+    # ec2-deploy/.media/user 폴더에 업로드한 파일이 저장되도록 설정
+    #
+    img_profile = models.ImageField(upload_to='user', blank=True)
+
