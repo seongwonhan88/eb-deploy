d2c0ac74dea02ecd4fb2b81893e01301380b8724
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-05 11:51:06 +0900

    .secrets/production.json added new postgresql db created "ec2_deploy_dev" for dev

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -18,8 +18,6 @@ MEDIA_URL = '/media/'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 secrets = json.load(open(os.path.join(SECRETS_DIR, 'base.json')))
-FACEBOOK_APP_ID = secrets['FACEBOOK_APP_ID']
-FACEBOOK_APP_SECRET = secrets['FACEBOOK_APP_SECRET']
 SECRET_KEY = 'secrets[SECRET_KEY]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
