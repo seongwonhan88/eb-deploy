29b7e9ea9e009b341f5044d3aec0c50c53c32a54
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-05 16:35:54 +0900

    boto3 s3 new bucket-production, DEFAULT_FILE_STORAGE setup

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -4,3 +4,7 @@ secrets = json.load(open(os.path.join(SECRETS_DIR, 'production.json')))
 DEBUG = False
 WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = secrets['DATABASES']
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME= secrets['AWS_STORAGE_BUCKET_NAME']
