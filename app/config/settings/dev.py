d1ca799b98f2cccb2d209d4943671b633f5e5cd9
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-06 11:12:45 +0900

    StaticStorage implemented, default_acl set to private/public

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -5,6 +5,8 @@ DEBUG = True
 WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = secrets['DATABASES']
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+# STATICFILES_STORAGE = 'config.storages.StaticStorage'
+
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME= secrets['AWS_STORAGE_BUCKET_NAME']
--- app/config/storages.py
+++ app/config/storages.py
@@ -3,3 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
+    default_acl = 'private'
+
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'public-read'
